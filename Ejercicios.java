6fb103aa89a1ea1a84cef11c82eef570d7d1ea84
08juaan <juanparedes7658@gmail.com>
2024-08-06 11:48:00 +0000

    Ejercicio de detectar pares entre 15 numeros ingresados con los tres bucles

--- Ejercicios.java
+++ Ejercicios.java
@@ -6,29 +6,45 @@ public class Ejercicios {
 	public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
 
-        int num = 0, suma =0;
+        int num = 0, par =0, impar=0;
         
-        /*for(int i=0;i<20;i++ ){
+        /*for(int i=0;i<15;i++ ){
                 System.out.println("Ingrese un numero entero");
 	         int numero = sc.nextInt();
-                suma = numero + suma;
+                
+                if (numero%2 == 0){
+                par = par + 1; 
+                }
+                else{
+                impar = impar +1;
+                }
         }
-        do{
-         System.out.println("Ingrese un numero entero");
-	         int numero = sc.nextInt();
+         do{
+        System.out.println("Ingrese un numero entero");
+         int numero = sc.nextInt();
+               
+               if (numero%2 == 0){
+                par = par + 1; 
+                }
+               else{
+                impar = impar +1;
+                }
+               num++;
+        }while (num < 15);
 
-            num++;
-            suma = numero + suma;
-        }while (num < 20);
-
-        while (num < 20){
-            System.out.println("Ingrese un numero entero");
-	            int numero = sc.nextInt();
-
-            num++;
-            suma = numero + suma;
-        }*/
-        System.out.println("El reultado de todos los numeros es " + suma);
+        while (num < 15){
+                System.out.println("Ingrese un numero entero");
+                int numero = sc.nextInt();
+                      
+                      if (numero%2 == 0){
+                       par = par + 1; 
+                       }
+                      else{
+                       impar = impar +1;
+                       }
+                      num++;
+        }
+        System.out.println("Ingreso " + par + " numeros pares" + " y " + impar + " impares");*/
 
 	}
 

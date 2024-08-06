b1ef2d8ee7c1a79dbb9e71ed71456af53f614ed2
08juaan <juanparedes7658@gmail.com>
2024-08-06 11:37:10 +0000

    Aqui un ejercio de java de pedir 20 numeros y sumarlos con for, while y do while

---
+++ Ejercicios.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+public class Ejercicios {
+
+	public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+        int num = 0, suma =0;
+        
+        /*for(int i=0;i<20;i++ ){
+                System.out.println("Ingrese un numero entero");
+	         int numero = sc.nextInt();
+                suma = numero + suma;
+        }
+        do{
+         System.out.println("Ingrese un numero entero");
+	         int numero = sc.nextInt();
+
+            num++;
+            suma = numero + suma;
+        }while (num < 20);
+
+        while (num < 20){
+            System.out.println("Ingrese un numero entero");
+	            int numero = sc.nextInt();
+
+            num++;
+            suma = numero + suma;
+        }*/
+        System.out.println("El reultado de todos los numeros es " + suma);
+
+	}
+
+}
